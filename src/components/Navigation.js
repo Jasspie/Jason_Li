c5d2dc30c6062d20e720e150d37d01b86ae91bce
Jasspie <lijason02@hotmail.com>
2021-08-02 13:25:47 -0400

    Updated Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,26 +2,35 @@ import React, { useEffect, useState } from "react"
 import { Container, Image, Nav, Navbar } from "react-bootstrap"
 import { useLocation } from "@reach/router"
 import styled from "styled-components"
-import Logo from "../assets/Logo.svg"
 
 const StyledNavbar = styled(Navbar)`
   opacity: ${({ show }) => (show ? 1 : 0)};
   transition: opacity 0.3s linear;
   font-family: Source Sans Pro, sans-serif;
-  background-color: white;
+  background-color: ${({ theme }) => theme.background};
   z-index: 10000;
 `
 
 const StyledNavbarLink = styled(Nav.Link)`
   opacity: 0.4;
   font-weight: 600;
-  color: black !important;
+  color: ${({ theme }) => theme.text} !important;
   cursor: pointer;
   margin-left: 1.5rem;
   transition: opacity 0.3s linear;
   &:hover {
     opacity: 1;
     transition: opacity 0.3s linear;
+  }
+`
+
+const StyledLogo = styled.svg`
+  height: 50px;
+  & > circle {
+    stroke: ${({ theme }) => theme.name};
+  }
+  & > path {
+    fill: ${({ theme }) => theme.name};
   }
 `
 
@@ -65,7 +74,30 @@ export default function Navigation() {
     <StyledNavbar expand="lg" fixed="top" show={active ? 1 : 0}>
       <Container>
         <Navbar.Brand href="#home">
-          <Image src={Logo} alt="Jason Li" style={{ height: 50 }} />
+          <StyledLogo
+            width="274"
+            height="274"
+            viewBox="0 0 274 274"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <circle
+              cx="137"
+              cy="137"
+              r="133"
+              fill="none"
+              stroke="#AD94CD"
+              strokeWidth="8"
+            />
+            <path
+              d="M126.544 101.488V172.048C126.544 181.84 123.712 189.472 118.048 194.944C112.384 200.32 104.8 203.008 95.296 203.008C85.696 203.008 77.92 200.224 71.968 194.656C66.112 189.088 63.184 181.312 63.184 171.328H83.344C83.44 175.648 84.448 179.008 86.368 181.408C88.384 183.808 91.264 185.008 95.008 185.008C98.656 185.008 101.44 183.856 103.36 181.552C105.28 179.248 106.24 176.08 106.24 172.048V101.488H126.544ZM170.221 186.016H203.341V202H150.061V101.488H170.221V186.016Z"
+              fill="#AD94CD"
+            />
+            <path
+              d="M88.488 81.904L189 81.904V102.064L88.488 102.064V81.904Z"
+              fill="#AD94CD"
+            />
+          </StyledLogo>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
