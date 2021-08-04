86db9b8cbae970f16310511440240315171a6f81
Jasspie <lijason02@hotmail.com>
2021-08-03 23:42:22 -0400

    Implemented local storage for theme selector

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,15 +3,13 @@ import Navigation from "./Navigation"
 import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from "../themes/GlobalStyles"
 import { lightTheme, darkTheme } from "../themes/Themes"
+import { useDarkMode } from "../themes/useDarkMode"
 
 export default function Layout({ children }) {
-  const [theme, setTheme] = useState("dark")
+  const [theme, themeToggler] = useDarkMode()
+
   if (typeof window != "undefined") {
     require("smooth-scroll")('a[href*="#"]')
-  }
-
-  const themeToggler = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light")
   }
 
   return (
