4f3108687c238c7bd70c3e63019ff04ad18dcae4
Jasspie <lijason02@hotmail.com>
2021-08-08 11:45:49 -0400

    Updated constants

---
+++ src/constants/Projects.js
@@ -0,0 +1,42 @@
+import Project1 from "../assets/Project1.png"
+
+export const PROJECT_IMAGES = [Project1, Project1, Project1]
+
+export const PROJECTS = [
+  {
+    id: 1,
+    image: Project1,
+    title: "Hi There",
+    description: "This is my project.",
+    stack: ["JavaScript", "React", "Python"],
+    repo: "https://google.com",
+    link: "https://google.com",
+    opacity: 0,
+    x: "-5rem",
+    ref: null,
+  },
+  {
+    id: 1,
+    image: Project1,
+    title: "Hi There",
+    description: "This is my project.",
+    stack: ["JavaScript", "React", "Python"],
+    repo: "https://google.com",
+    link: "https://google.com",
+    opacity: 0,
+    x: "-5rem",
+    ref: null,
+  },
+  {
+    id: 1,
+    image: Project1,
+    title: "Hi There",
+    description: "This is my project.",
+    stack: ["JavaScript", "React", "Python"],
+    repo: "https://google.com",
+    link: "https://google.com",
+    opacity: 0,
+    x: "-5rem",
+    ref: null,
+  },
+]
--- src/constants/Skills.js
+++ src/constants/Skills.js
@@ -1,5 +1,3 @@
-import React from "react"
-
 function importAll(r) {
   let images = {}
   r.keys().forEach((item, index) => {
