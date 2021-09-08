ec9b0bdddbaf2ac89b18370718e87de53301c0e2
Jason Li <lijason02@hotmail.com>
2021-09-08 14:44:50 -0400

    Updated projects

--- src/constants/Projects.js
+++ src/constants/Projects.js
@@ -1,49 +1,52 @@
-import Project1 from "../assets/projects/Project1.png"
+import Project1 from "../assets/projects/SETHacks.png"
+import Project2 from "../assets/projects/ENergize.png"
+import Project3 from "../assets/projects/Shopperz.png"
+import Project4 from "../assets/projects/PersonalWebsite.png"
+import Project5 from "../assets/projects/CaseMap.png"
 
 export const PROJECTS = [
   {
-    title: "P R O J E C T",
+    title: "SET.Hacks()",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas diam ut tortor finibus porta. Etiam vulputate orci vitae scelerisque cursus. Aenean semper vehicula bibendum. Vestibulum tempus libero eu nunc elementum, a mattis ipsum convallis. ",
-    stack: ["JavaScript", "React", "Python"],
-    repo: "https://google.com",
-    link: "https://google.com",
+      "Through SET.Hacks(), I collaborated with a team of developers to re-brand the current website and establish a beginner-friendly experience for participating hackers. As an organizer, I had an unforgettable experience in making SET.Hacks() a success and acquired many skills through building the website.",
+    stack: ["TypeScript", "React", "Next.js", "Styled-Components"],
+    repo: "https://github.com/SET-Hacks-Dev/set-hacks",
+    link: "https://www.sethacks.ca/",
     image: Project1,
   },
   {
-    title: "P R O J E C T",
+    title: "ENergize",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas diam ut tortor finibus porta. Etiam vulputate orci vitae scelerisque cursus. Aenean semper vehicula bibendum. Vestibulum tempus libero eu nunc elementum, a mattis ipsum convallis. ",
-    stack: ["JavaScript", "React", "Python"],
-    repo: "https://google.com",
-    link: "https://google.com",
-    image: Project1,
+      "Originally as an idea for a Hackathon, ENergize ended up being a project I developed over the summer. It's a full stack application that allows users to create recipes and track them in terms of its environmental sustainability and nutritional benefits. Try creating a recipe and see the ENergize score you'll receive.",
+    stack: ["React", "Firebase", "Python", "OpenAI API", "Beautiful Soup"],
+    repo: "https://github.com/Jasspie/ENergize",
+    link: "https://energize.netlify.app/",
+    image: Project2,
   },
   {
-    title: "P R O J E C T",
+    title: "Shopperz",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas diam ut tortor finibus porta. Etiam vulputate orci vitae scelerisque cursus. Aenean semper vehicula bibendum. Vestibulum tempus libero eu nunc elementum, a mattis ipsum convallis. ",
-    stack: ["JavaScript", "React", "Python"],
-    repo: "https://google.com",
-    link: "https://google.com",
-    image: Project1,
+      "As a tech enthusiast, I created Shopperz, a MERN web application that serves as a mock e-commerce platform for users to purchase various devices. Through building this project, I learned how to apply Node.js and MongoDB towards authenticating users with JSON Web Tokens as well as how to handle payments with the Stripe API.",
+    stack: ["Node.js", "Express", "MongoDB", "Stripe API", "React", "Redux"],
+    repo: "https://github.com/Jasspie/Shopperz",
+    image: Project3,
   },
   {
-    title: "P R O J E C T",
+    title: "Personal Website",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas diam ut tortor finibus porta. Etiam vulputate orci vitae scelerisque cursus. Aenean semper vehicula bibendum. Vestibulum tempus libero eu nunc elementum, a mattis ipsum convallis. ",
-    stack: ["JavaScript", "React", "Python"],
-    repo: "https://google.com",
-    link: "https://google.com",
-    image: Project1,
+      "You’re currently on this website! Throughout the course of this project, I learned how to apply Gatsby and Strapi as a headless CMS. My favorite part about the building process was using Framer Motion and custom React Hooks to handle all the animations on this page.",
+    stack: ["Gatsby", "React", "GraphQL", "Strapi", "Framer Motion"],
+    repo: "https://github.com/Jasspie/Jason_Li",
+    link: "https://lijason.me/",
+    image: Project4,
   },
   {
-    title: "P R O J E C T",
+    title: "CaseMap",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce egestas diam ut tortor finibus porta. Etiam vulputate orci vitae scelerisque cursus. Aenean semper vehicula bibendum. Vestibulum tempus libero eu nunc elementum, a mattis ipsum convallis. ",
-    stack: ["JavaScript", "React", "Python"],
-    repo: "https://google.com",
-    link: "https://google.com",
-    image: Project1,
+      "In the midst of the COVID-19 pandemic, I was inspired to spend my free time learning as many new things as I could. Shortly afterwards, my very first React application, CaseMap, was born! It's an interactive map which presents the current and historical COVID-19 data through an interactive map and charts. Feel free to play around with it!",
+    stack: ["React", "Chart.js", "Leaflet"],
+    repo: "https://github.com/Jasspie/CaseMap",
+    link: "https://jasspie.github.io/CaseMap/",
+    image: Project5,
   },
 ]
